35adf2565dd5059af13d87305f36d92b7099195c
Paulo Silvino <paulosilvino23@hotmail.com>
2024-02-02 13:11:48 -0500

    Minimum budget value validation

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -3,14 +3,24 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
+    const { expenses } = useContext(AppContext);
     const { budget } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
     const handleBudgetChange = (event) => {
         
+        const totalExpenses = expenses.reduce((total, item) => {
+            return (total = total + item.cost); }, 0);
+
         if(event.target.value > 20000) {
             alert("Value budget max exceeded!");
             return;
         }
+
+        if(event.target.value < totalExpenses) {
+            alert("You cannot reduce the budget value lower than the spending");
+            return;
+        }
+        
         setNewBudget(event.target.value);
     }
     return (
